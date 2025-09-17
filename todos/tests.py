ab5960ca0e8a9b6a8151cee7e72744378897aa56
paulebil <ebilpaul06@gmail.com>
2025-09-17 09:42:03 +0300

    created test cases for our api endpoints

--- todos/tests.py
+++ todos/tests.py
@@ -1,10 +1,11 @@
-from django.test import TestCase
-
-# Create your tests here.
-
+import json
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
 from .models import Todos
 
-class TodoModelTest(TestCase):
+
+class TodoModelTest(APITestCase):
     @classmethod
     def setUpTestData(cls):
         cls.todo = Todos.objects.create(
@@ -16,3 +17,23 @@ class TodoModelTest(TestCase):
         self.assertEqual(self.todo.title, "First Todo")
         self.assertEqual(self.todo.body, "A body of text here")
         self.assertEqual(str(self.todo), "First Todo")
+
+    def test_api_list_view(self):
+        response = self.client.get(reverse("todo_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todos.objects.count(), 1)
+        data = json.loads(response.content)
+        self.assertEqual(data[0]["title"], self.todo.title)
+        self.assertEqual(data[0]["body"], self.todo.body)
+
+    def test_api_detail_view(self):
+        response = self.client.get(
+            reverse("todo_detail", kwargs={"pk": self.todo.pk}),
+            format="json"
+        )
+        data = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todos.objects.count(), 1)
+
+        self.assertEqual(data["title"], "First Todo")
+        self.assertEqual(data["body"], "A body of text here")
--- todos/urls.py
+++ todos/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ListTodo, DetailTodo
 
 urlpatterns = [
-    path("<int:pk>/", DetailTodo.as_view(), name="todo-detail" ),
+    path("<int:pk>/", DetailTodo.as_view(), name="todo_detail" ),
     path("", ListTodo.as_view(), name="todo_list"),
 ]
