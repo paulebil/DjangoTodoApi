0795267216d3ca4bc296d8aa4f47d41c106b3ed9
paulebil <ebilpaul06@gmail.com>
2025-09-17 09:49:21 +0300

    added corsheaders middleware to settings.py file

--- todapi_project/settings.py
+++ todapi_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd party
     'rest_framework',
+    'corsheaders',
     # locals
     'todos.apps.TodosConfig',
 ]
@@ -50,13 +51,17 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://localhost:3000",
+]
 ROOT_URLCONF = 'todapi_project.urls'
 
 TEMPLATES = [
