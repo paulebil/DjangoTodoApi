70d35ac471e18c795be3443720ce4cabeab7e3f7
paulebil <ebilpaul06@gmail.com>
2025-09-17 08:32:33 +0300

    created todo model and superuser

--- todapi_project/settings.py
+++ todapi_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # locals
-    'todos.app.TodosConfig',
+    'todos.apps.TodosConfig',
 ]
 
 MIDDLEWARE = [
--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Todos
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = {
+        "title",
+        "body"
+    }
+
+
+admin.site.register(Todos)
